4db7a57e03f15d3ce118b3bd1e1f85e16f4b2946
Pavol Gál <palid1@gmail.com>
2016-02-23 01:43:37 +0100

    method refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.Scanner;
 
@@ -6,32 +7,27 @@ public class Main {
 
 	public static void main(String[] arguments) {
 
-
 		Scanner scanner = new Scanner(System.in);
 
-		String line1 = scanner.next();
-		int number1 = 0;
-		if (StringUtils.isNumeric(line1)) {
-			number1 = Integer.parseInt(line1);
-		} else {
-			System.out.println("not a number");
-			System.exit(0);
-		}
-
-
-		String line2 = scanner.next();
-		int number2 = 0;
-		if (StringUtils.isNumeric(line2)) {
-			number2 = Integer.parseInt(line2);
-		} else {
-			System.out.println("not a number");
-			System.exit(0);
-		}
+		int number1 = convertStringToNumber(scanner.next());
+		int number2 = convertStringToNumber(scanner.next());
 
 		int result = number1 + number2;
 
 		System.out.println(result);
 
 	}
+
+	public static int convertStringToNumber(String line) {
+		int number = 0;
+		if(StringUtils.isNumeric(line)) {
+			number = Integer.parseInt(line);
+		} else {
+			System.out.println("not a number");
+			System.exit(0);
+		}
+		return number;
+	}
+
 
 }
