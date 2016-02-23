468262c4cb48ddfa296d84922ebd77b09ca9fad1
Pavol Gál <palid1@gmail.com>
2016-02-23 01:37:39 +0100

    first calculator

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] arguments) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int number1 = scanner.nextInt();
+		int number2 = scanner.nextInt();
+
+		int result = number1 + number2;
+
+		System.out.println(result);
+
+	}
+
+}
