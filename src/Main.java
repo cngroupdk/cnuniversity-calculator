f0c12ffc7ed0917c31614e369208e36b911a84d4
Pavol Gál <palid1@gmail.com>
2016-02-27 12:54:19 +0100

    checking and parsing number

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] arguments) {
 
+
 		Scanner scanner = new Scanner(System.in);
 
-		int number1 = scanner.nextInt();
-		int number2 = scanner.nextInt();
+		String line1 = scanner.next();
+		int number1 = 0;
+		if (StringUtils.isNumeric(line1)) {
+			number1 = Integer.parseInt(line1);
+		} else {
+			System.out.println("not a number");
+			System.exit(0);
+		}
+
+
+		String line2 = scanner.next();
+		int number2 = 0;
+		if (StringUtils.isNumeric(line2)) {
+			number2 = Integer.parseInt(line2);
+		} else {
+			System.out.println("not a number");
+			System.exit(0);
+		}
 
 		int result = number1 + number2;
 
